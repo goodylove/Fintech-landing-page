4979562dae7d96ee30cbdf5102a0e3be9682d918
goodness <goodyc474@gmail.com>
2023-03-04 12:38:51 +0100

    created nav bar components

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
   return (
-    <div className="">
+    <div className="flex items-center justify-between bg-white text-blue-600   w-full p-5  shadow-inner text-3xl">
       <div className="log">
-        <h1>Fintech</h1>
+        <h4>Fintekk</h4>
       </div>
       <div className="menu">
-        <IconAlarmOff />
+        <AiOutlineMenu className="text-3xl" />
       </div>
     </div>
   );
