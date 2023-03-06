b799cd0bd5e99ee36a666e6583cb9e584a06522d
goodness <goodyc474@gmail.com>
2023-03-06 12:38:00 +0100

    ft:completed navbar ui

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,25 +5,26 @@ const NavBar = () => {
   return (
     <div className="flex items-center justify-between bg-white text-blue-600   w-full p-5  shadow-md text-3xl">
       <div className="log">
-        <h4>Fintekk</h4>
+        <h5 className="text-lg font-bold">Fintekk</h5>
       </div>
 
-      <div className="menulist">
-        <ul>
+      <div className="menulist w-[50%]">
+        <ul className="md:flex justify-around items-center text-sm text-gray-500 hidden font-bold">
           <li>Home</li>
           <li>About us</li>
           <li>FQAS</li>
           <li>Contact Us</li>
         </ul>
       </div>
-      <div>
-        <button className=" bg-white text-xl text-blue-500 w-30 border-2  border-solid">
+      <div className="w-[40%] flex justify-around">
+        <button className="hidden md:flex bg-white  text-center justify-center text-sm text-blue-500 w-[80px] border-2  border-solid rounded-lg p-2 font-bold">
           Login
         </button>
+        <button className="bg-blue-700 justify-center text-white w-[160px] p-1 rounded-lg text-[15px] font-bold hidden md:flex">
+          Sign up for free
+        </button>
       </div>
-      <div className="login-btn">
-        <button>Sign In</button>
-      </div>
+
       <div className="md:hidden">
         <AiOutlineMenu className="text-3xl" />
       </div>
