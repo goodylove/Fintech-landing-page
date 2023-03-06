8443b6d8610b3847d7bafa7abf13773cb3a8c4b1
goodness <goodyc474@gmail.com>
2023-03-06 11:59:02 +0100

    ft:created button components

---
+++ src/components/Contenttext.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const ContextText = () => {
+  return (
+    <div>
+      <p>Pay,Invest,Save your cah with a single bank</p>
+    </div>
+  );
+};
+
+export default ContextText;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,11 +3,28 @@ import { AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between bg-white text-blue-600   w-full p-5  shadow-inner text-3xl">
+    <div className="flex items-center justify-between bg-white text-blue-600   w-full p-5  shadow-md text-3xl">
       <div className="log">
         <h4>Fintekk</h4>
       </div>
-      <div className="menu">
+
+      <div className="menulist">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>FQAS</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div>
+        <button className=" bg-white text-xl text-blue-500 w-30 border-2  border-solid">
+          Login
+        </button>
+      </div>
+      <div className="login-btn">
+        <button>Sign In</button>
+      </div>
+      <div className="md:hidden">
         <AiOutlineMenu className="text-3xl" />
       </div>
     </div>
