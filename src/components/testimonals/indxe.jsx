deccbedbd88b027b943600e987512701de40c45b
goodness <goodyc474@gmail.com>
2023-03-24 16:28:26 +0100

    complete testimonal

--- src/components/testimonals/indxe.jsx
+++ src/components/testimonals/indxe.jsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Testimonial from "./testimonal";
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
 
 import { BsArrowDownShort } from "react-icons/bs";
 import { AiOutlineArrowUp } from "react-icons/ai";
 
 const testimonial = [
   {
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat qui voluptatum voluptatibus a error facere quia, minus laborum libero eveniet id nulla?",
+    text: "Lorem ipsum dolor sit amet . Repellat qui voluptatum voluptatibus a error facere quia, minus laborum libero eveniet id nulla?",
     name: "goodness",
     id: 1,
+  },
+  {
+    text: "ipsum dolor sit amet consectetur,ui voluptatum voluptatibus a error facere quia, minus laborum libero eveniet id nulla?",
+    name: "esther",
+    id: 2,
+  },
+  {
+    text: "orem ipsum dolor sit amet consectetur, adipisicing elit.oluptatum voluptatibus a error facere quia, minus laborum libero eveniet id nulla?",
+    name: "joy",
+    id: 3,
   },
 ];
 
 const Testimonials = () => {
+  const [currentTestimonal, setCurrentTestimonal] = useState(0);
+  const [translation, setTransaction] = useState(false);
+  let testimonalLength = testimonial.length;
+
+  const upArrow = () => {
+    setCurrentTestimonal(
+      currentTestimonal === testimonalLength - 1 ? 0 : currentTestimonal + 1
+    );
+    setTransaction(true);
+    console.log(testimonalLength, currentTestimonal);
+  };
+  const downArrow = () => {
+    setCurrentTestimonal(
+      currentTestimonal === 0 ? testimonalLength - 1 : currentTestimonal - 1
+    );
+  };
+  useEffect(() => {
+    setCurrentTestimonal(0);
+  }, []);
+
   return (
     <div className="flex  mt-10 bg-gray-100 w-full p-10">
       <div className="flex justify-between w-[90%] items-center  flex-col md:flex-row">
@@ -22,16 +54,48 @@ const Testimonials = () => {
             What Our Customers Says About Us
           </p>
         </div>
-        <Testimonial />
+        {testimonial.map(
+          (testimonal, index) =>
+            index === currentTestimonal && (
+              <Testimonial
+                testimonal={testimonal}
+                key={index}
+                translation={translation}
+              />
+            )
+        )}
 
         <div className="flex md:flex-col text-blue-800  items-center md:w-[20%] flex-row w-[40%] m-5 justify-between  ">
-          <AiOutlineArrowUp className="mb-1" />
-          <div className="bg-blue-500 h-5 w-5 rounded-full text-[13px] text-white">
+          <AiOutlineArrowUp className="mb-1" onClick={upArrow} />
+          <div
+            className={
+              currentTestimonal === 0
+                ? "bg-blue-500 h-5 w-5 rounded-full text-[13px] text-white"
+                : ""
+            }
+          >
             1
           </div>
-          <div>2</div>
-          <div>3</div>
-          <BsArrowDownShort className="mt-1" />
+          <div
+            className={
+              currentTestimonal === 1
+                ? "bg-blue-500 h-5 w-5 rounded-full text-[13px] text-white"
+                : ""
+            }
+          >
+            2
+          </div>
+          <div
+            className={
+              currentTestimonal === 2
+                ? "bg-blue-500 h-5 w-5 rounded-full text-[13px] text-white"
+                : ""
+            }
+          >
+            3
+          </div>
+
+          <BsArrowDownShort className="mt-1" onClick={downArrow} />
         </div>
       </div>
     </div>
--- src/components/testimonals/testimonal.jsx
+++ src/components/testimonals/testimonal.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 import { AiOutlineStar } from "react-icons/ai";
-const Testimonial = () => {
+const Testimonial = ({ testimonal }) => {
   return (
-    <div className="md:w-[40%]  w-[90%]text-start bg-white  rounded p-2 m-5">
-      <p className="text-[13px]">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat qui
-        voluptatum voluptatibus a error facere quia, minus laborum libero
-        eveniet id nulla?
-      </p>
-      <h3 className="text-blue-900 mt-2 mb-2">Glory nwaimoh</h3>
+    <div className={`md:w-[72%]  w-[90%]text-start bg-white  rounded p-2 m-5 `}>
+      <p className="text-[13px]">{testimonal.text}</p>
+      <h3 className="text-blue-900 mt-2 mb-2">{testimonal.name}</h3>
       <AiFillStar className="icon-start" />
       <AiFillStar className="icon-start" />
       <AiFillStar className="icon-start" />
